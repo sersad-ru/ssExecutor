499c87a72ef64265764ea0a9a0aedb2f26944718
sersad <51073352+sersad-ru@users.noreply.github.com>
2025-04-05 18:12:26 +0300

    Update ssExecutor.cpp

--- src/ssExecutor.cpp
+++ src/ssExecutor.cpp
@@ -3,7 +3,7 @@
 void ssExecutor::exec(uint32_t delay_ms, ssPayload func){
   if(!delay_ms) delay_ms = _delay_ms;
   static uint32_t last_action = millis();
-  if((millis() - last_action) < delay_ms) return;
+  if(delay_ms & ((millis() - last_action) < delay_ms)) return;
   last_action = millis();
 
   if(!func) func = _func;
